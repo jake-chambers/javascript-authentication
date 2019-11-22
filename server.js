1e58bce6aefe435023a8992d9405dc85d6d8878e
Jacob <jake_chambers12@hotmail.com>
2019-11-22 15:06:46 -0500

    sending creds over https wootttt hack me now hoe

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const parser = require('body-parser')
-const app = express();
+const fs = require('fs');
+var https = require('https')
 const port = process.env.PORT || 3000;
+
+const app = express();
 app.use(express.static('static'))
 app.use(parser.json());
 
-app.listen(port, () => {
-  console.log(`listening on port ${ port }`);
-});
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+app.post('/authenticate', function(req,res){
+  res.send("we livin famo")
+})
+
+https.createServer(options, app)
+.listen(3000, function () {
+  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+})
--- static/script.js
+++ static/script.js
@@ -1,13 +1,30 @@
 const authenticate = () => {
     let username = document.getElementById("username").value
     let password = document.getElementById("password").value
-    validateInputs(username,password)
+
     console.log("trying to authenticate..")
+
+    if (validateInputs(username, password)) {
+        axios({
+            method: 'post',
+            url: '/authenticate',
+            data: {
+                username: username,
+                password: password
+            },
+        })
+        .then(function (response) {
+            alert("successfully sent request")
+        })
+    }
+
 }
 
 
 const validateInputs = (username, password) => {
-    if (!username || !password){
+    if (!username || !password) {
         alert("fix your inputs!!")
+        return false;
     }
+    return true;
 }   
